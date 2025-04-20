b16441dbed4c82cbd3992100fc2e7ecb21542684
nate-loz <nathan.lozinsky@gmail.com>
2025-04-20 18:11:20 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -14,5 +14,25 @@ function populateGradebook(data) {
 // Call the stubs to demonstrate the workflow
 const gradeData = fetchGradeData();
 populateGradebook(gradeData);
+function fetchGradeData(){
+		    // This function will query the PostgreSQL database and return grade data
+		    console.log("Fetching grade data...");
+		    // Create a new request for HTTP data
+		    let xhr = new XMLHttpRequest();
+		    // This is the address on the machine we're asking for data
+		    let apiRoute = "/api/grades";
+		    // When the request changes status, we run this anonymous function
+		    xhr.onreadystatechange = function(){
+			    let results;
+			    // Check if we're done if
+			    if(xhr.status != 200){ console.eror('Could not get grades.
+				Status: ${xhr.status}');
+			    }
+			    // And then call the function to update the HTML with our data
+			    populateGradebook(JSON.parse(xhr.responseText));
+		    }.bind(his);
+		    xhr.open("get", apiRoute, true);
+		    xhr.send();
+	    }
 // END REMOVE
 
